2ef968c0ac0457d09f90fdb50a15abaa740aaa82
nuriknorik <110288380+nuriknorik@users.noreply.github.com>
2022-09-21 14:09:01 +0400

    Add files via upload

---
+++ Convert.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Client
+    {
+        public const string _key = "P4UwunHfmFXlLEpF2rSiQ1vtp1WeobCi";
+        public const string _url = @"https://api.apilayer.com/exchangerates_data/convert?";
+        public static Converter Converter(string first ,string second , float count)
+        {
+            var client = new WebClient();
+            var Downoload = client.DownloadString($"{_url}apikey={_key}&to={first}&from={second}&amount={count}");
+            throw new NullReferenceException();
+        }
+    }
+}
---
+++ Converter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp5
+{
+    internal class Converter
+    {
+        public Query query { get; set; }
+        public float result { get; set; }
+
+
+        public class Query
+        {
+            public float amount { get; set; }
+            public string from { get; set; }
+            public string to { get; set; }
+        }
+        public override string ToString() => $"{query.from}: {query.amount} == {query.to}: {result}";
+    }
+}
